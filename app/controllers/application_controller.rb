d2887564b3cfaaff921d1a74c3eb46fb3e966885
Alexey Tataurov <zealav@gmail.com>
2016-01-19 11:52:09 +0500

    issue(routs): add 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActionController::RoutingError, with: -> { not_found }
+    rescue_from ActiveRecord::RecordNotFound, with: -> { not_found  }
+  end
+
+  def not_found
+    render file: 'public/404.html', status: :not_found, layout: false
+  end
+
   layout :layout_by_resource
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   root 'root#index'
 
   resources :works, only: [:index]
+
+  get '*unmatched_route', to: 'application#not_found'
 end
