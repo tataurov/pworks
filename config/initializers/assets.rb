aea68b4a53925c7d9aeff4ecd0dd13cbcbabcf73
Alexey Tataurov <zealav@gmail.com>
2016-01-23 17:37:07 +0500

    fix(assets): add selective compressor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+require_relative '../../lib/assets/selective_assets_compressor'
+Rails.application.config.assets.js_compressor = SelectiveAssetsCompressor.new
+
 folders = %w( package/ active_admin.css ckeditor/* )
 
 Rails.application.config.assets.precompile << Proc.new do |path|
